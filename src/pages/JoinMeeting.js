6ec281458f093620e7d3552d12e3ec572ce3e9d1
Mehtab Alam <mehtaba728@mail.com>
2021-09-21 16:37:26 +0500

    added the join-meeting form

--- src/pages/JoinMeeting.js
+++ src/pages/JoinMeeting.js
@@ -1,11 +1,26 @@
 import React from "react";
+import "./joinmeeting.css";
 
 const JoinMeeting = () => {
   return (
-    <div>
-      <h1>Welcome</h1>
-      <h2>To Join Meeting Page</h2>
-    </div>
+    <form className="container">
+      <div>
+        <h5>Join Meeting</h5>
+        <br />
+        <p>Share the meeting link to invite others</p>
+      </div>
+      <div className="form-group">
+        <input type="text" className="form-control" />
+      </div>
+      <br />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Please enter your name"
+      />
+      <br />
+      <button className="btn btn-primary btn-block">Join meeting</button>
+    </form>
   );
 };
 
